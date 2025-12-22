2d88f317574573a97719ea82cf9718bd5bae7cce
ilyass chekhmane <166509791+azizq01a@users.noreply.github.com>
2025-12-22 23:43:43 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// js/script.js
+document.querySelectorAll('.btn').forEach(btn=>{
+    btn.addEventListener('mouseover', ()=> btn.style.transform='scale(1.05)');
+    btn.addEventListener('mouseout', ()=> btn.style.transform='scale(1)');
+});
